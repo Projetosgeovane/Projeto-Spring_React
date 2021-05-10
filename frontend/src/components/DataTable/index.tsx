a415b2ca554abdede61fe45559b0f8c1a5f692d7
Geovane Silva <geovanesilva.nar@hotmail.com>
2021-05-10 01:36:02 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,62 +1,66 @@
+import axios from "axios";
+import Pagination from "components/Pagination";
+
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/request";
+
 const DataTable = () => {
+
+    const[activePage, setActivePage] = useState(0);
+    
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=20sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            });
+    }, [activePage]);
+    
+        const changePage = (index: number) => {
+            setActivePage(index);
+        }
+
     return (
-        <div className="table-responsive">
-            <table className="table table-striped table-sm">
-                <thead>
-                    <tr>
-                        <th>Data</th>
-                        <th>Vendedor</th>
-                        <th>Clientes visitados</th>
-                        <th>Negócios fechados</th>
-                        <th>Valor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Geovane Silva</td>
-                        <td>22</td>
-                        <td>20</td>
-                        <td>15050.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Carlos Andrews</td>
-                        <td>24</td>
-                        <td>22</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Breno Tomas</td>
-                        <td>40</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Marcos Tino</td>
-                        <td>34</td>
-                        <td>23</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Tomas Nicolas</td>
-                        <td>41</td>
-                        <td>19</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <>  
+            <Pagination page={page} onPageChange={changePage}/>
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Vendedor</th>
+                            <th>Clientes visitados</th>
+                            <th>Negócios fechados</th>
+                            <th>Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {page.content?.map(item => (
+                            <tr key={item.id}>
+                                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                                <td>{item.seller.name}</td>
+                                <td>{item.visited}</td>
+                                <td>{item.deals}</td>
+                                <td>{item.amount.toFixed(2)}</td>
+                            </tr>
+                        ))}
+                        <tr>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
